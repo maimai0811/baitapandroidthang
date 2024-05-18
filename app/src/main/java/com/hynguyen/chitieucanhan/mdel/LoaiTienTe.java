b32748bbaefa6376f13fb8f16157193abd3414c5
maimai <maimaint.vn@gmail.com>
2024-05-18 14:56:50 +0700

    init

---
+++ app/src/main/java/com/hynguyen/chitieucanhan/mdel/LoaiTienTe.java
@@ -0,0 +1,45 @@
+package com.hynguyen.chitieucanhan.mdel;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "tb_money_type")
+
+public class LoaiTienTe {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "money_id")
+    private int id;
+    @ColumnInfo(name = "money_name")
+    private String name;
+
+    public LoaiTienTe() {
+    }
+    @Ignore
+    public LoaiTienTe(String name) {
+        this.name = name;
+    }
+
+    @Ignore
+    public LoaiTienTe(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
